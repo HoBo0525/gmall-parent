256830a4301df50af0f1d54eef15cbe00c83ae7e
HoBo <1781194173@qq.com>
2021-02-10 12:37:12 +0800

    v2.1   新增功能（切面使用分布式锁、异步编排、首页数据放缓存）

--- web/web-all/src/main/java/com/atguigu/gmall/all/controller/ItemController.java
+++ web/web-all/src/main/java/com/atguigu/gmall/all/controller/ItemController.java
@@ -2,9 +2,11 @@ package com.atguigu.gmall.all.controller;
 
 import com.atguigu.gmall.common.result.Result;
 import com.atguigu.gmall.item.client.ItemFeignClient;
+import com.atguigu.gmall.product.client.ProductFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,11 +21,21 @@ public class ItemController {
     @Autowired
     ItemFeignClient itemFeignClient;
 
+    @Autowired
+    ProductFeignClient productFeignClient;
+
     @RequestMapping("{skuId}.html")
     public String getItem(@PathVariable Long skuId, Model model){
         Result<Map> result = itemFeignClient.getItem(skuId);
         model.addAllAttributes(result.getData());
 
         return "item/index";
+    }
+
+    @GetMapping({"/", "index.html"})
+    public String index(Model model){
+        Result result = productFeignClient.getBaseCategoryList();
+        model.addAttribute("list", result.getData());
+        return "index/index";
     }
 }
